fe06b8dbbd56a6e33dbb74d16b6deb3d10dc6abd
Thibault <thibault.schowing@heig-vd.ch>
2015-04-22 09:52:07 +0200

    Ajout des interfaces et classes principales

--- HttpLibrary-build/HttpLibrary-test/src/test/java/ch/heigvd/res/labs/http/impl/HttpClientTest.java
+++ HttpLibrary-build/HttpLibrary-test/src/test/java/ch/heigvd/res/labs/http/impl/HttpClientTest.java
@@ -1,10 +1,40 @@
 package ch.heigvd.res.labs.http.impl;
 
+import ch.heigvd.res.labs.http.interfaces.IHttpClient;
+import ch.heigvd.res.labs.http.interfaces.IHttpResponse;
+import java.io.IOException;
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+
 
 /**
  *
  * @author Olivier Liechti
  */
 public class HttpClientTest {
-  
+    @Test
+    public void itShouldBePossibleToSendAGETRequest() throws IOException{
+        IHttpClient client = new HttpClient();
+        HttpRequest request = new HttpRequest();
+        request.setMethod("GET");
+        request.setProtocolVersion("HTTP/1.1");
+        request.setURI("http://www.heig-vd.ch/presentation");
+        IHttpResponse response = client.sendRequest(request);
+        assertEquals(200,response.getStatusCode());
+    }
+    
+    /**
+     *
+     * @throws IOException
+     */
+    @Test
+    public void itShouldBePossibleToSendAGETRequest2() throws IOException{
+        IHttpClient client = new HttpClient();
+        HttpRequest request = new HttpRequest();
+        request.setMethod("GET");
+        request.setProtocolVersion("HTTP/1.1");
+        request.setURI("http://www.google.com/");
+        IHttpResponse response = client.sendRequest(request);
+        assertEquals(302,response.getStatusCode());
+    }
 }
